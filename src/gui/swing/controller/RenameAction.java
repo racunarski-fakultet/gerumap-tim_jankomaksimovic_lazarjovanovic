1446a7b79c54b98a43055c4e5edcc8c2b85ac8f7
WIN 10 <jmaksimovic11222@raf.rs>
2022-10-31 17:16:09 +0100

    zapocet skelet

---
+++ src/gui/swing/controller/RenameAction.java
@@ -0,0 +1,16 @@
+package gui.swing.controller;
+
+import java.awt.event.ActionEvent;
+
+public class RenameAction extends AbstractGeRuMapAction{
+
+    public RenameAction()
+    {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
